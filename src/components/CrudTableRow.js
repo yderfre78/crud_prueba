15175e4fd37b58d950b28402028564e3e0043ce9
yderfre78 <72670647+yderfre78@users.noreply.github.com>
2022-07-13 22:32:39 -0500

    creando tabla

---
+++ src/components/CrudTableRow.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export const CrudTableRow = () => {
+  return (
+    <tr>
+      <td>Juan</td>
+      <td>310-123-4561</td>
+      <td>20</td>
+      <td>juan@gmail.com</td>
+      <td>calle falsa 123</td>
+      <td>10/10/2002</td>
+      <td>
+        <button>Editar</button> <button>Eliminar</button>
+      </td>
+    </tr>
+  );
+};
