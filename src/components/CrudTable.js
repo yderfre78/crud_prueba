865b3e0cdca1decd85260cffd5b4b57ce7cec155
yderfre78 <72670647+yderfre78@users.noreply.github.com>
2022-07-14 11:37:49 -0500

    logica para busqueda y filtrado

--- src/components/CrudTable.js
+++ src/components/CrudTable.js
@@ -1,40 +1,120 @@
-import React from "react";
+import React, { useState } from "react";
 import { CrudTableRow } from "./CrudTableRow";
 
-export const CrudTable = ({ data, setDataToEdit, deleteData }) => {
+export const CrudTable = ({
+  data,
+  setDataToEdit,
+  deleteData,
+  setDb,
+  dbSearch,
+}) => {
+  const [search, setSearch] = useState("");
+  const [searchEmail, setSearchEmail] = useState("");
+  const [searchPhone, setSearchPhone] = useState("");
+  const handlesearcherName = (e) => {
+    setSearch(e.target.value);
+    console.log(e.target.value);
+    filterBy(e.target.value);
+  };
+  const handlesearcherEmail = (_e) => {
+    setSearchEmail(_e.target.value);
+    console.log(_e.target.value);
+    filterByEmail(_e.target.value);
+  };
+  const handlesearcherPhone = (_e) => {
+    setSearchPhone(_e.target.value);
+    console.log(_e.target.value);
+    filterByPhone(_e.target.value);
+  };
+
+  const filterBy = (searchTerm) => {
+    let resulSearch = dbSearch.filter((element) => {
+      if (
+        element.name.toString().toLowerCase().includes(searchTerm.toLowerCase())
+        // || elemento.gmail.toString().toLowerCase().includes(tsearchTerm.toLowerCase())
+      ) {
+        return element;
+      }
+    });
+    setDb(resulSearch);
+  };
+  const filterByEmail = (searchTerm) => {
+    let resulSearch = dbSearch.filter((element) => {
+      if (
+        element.email
+          .toString()
+          .toLowerCase()
+          .includes(searchTerm.toLowerCase())
+       
+      ) {
+        return element;
+      }
+    });
+    setDb(resulSearch);
+  };
+  const filterByPhone = (searchTerm) => {
+    let resulSearch = dbSearch.filter((element) => {
+      if (
+        element.phone
+          .toString()
+          .toLowerCase()
+          .includes(searchTerm.toLowerCase())
+        // || elemento.gmail.toString().toLowerCase().includes(tsearchTerm.toLowerCase())
+      ) {
+        return element;
+      }
+    });
+    setDb(resulSearch);
+  };
   return (
     <div>
-      <h3>Tabal de datos</h3>
-        <table>
-          <thead>
-            <tr>
-              <th>Nombre</th>
-              <th>Telefono</th>
-              <th>Edad</th>
-              <th>Email</th>
-              <th>Direccion</th>
-              <th>Fecha de Nacimiento</th>
-              <th>Acciones</th>
-            </tr>
-          </thead>
-          <tbody>
-            {data.length === 0 ? (
-              <tr>
-                <td colSpan={7}>Sin Datos</td>
-              </tr>
-            ) : (
-              data.map((el) => (
-                <CrudTableRow
-                  key={el.id}
-                  el={el}
-                  setDataToEdit={setDataToEdit}
-                  deleteData={deleteData}
-                />
-              ))
-            )}
-          </tbody>
-        </table>
-      
+      <input
+        className="form-control inputBuscar"
+        value={search}
+        onChange={handlesearcherName}
+        type="text"
+        placeholder="Búsqueda por Nombre"
+      ></input>
+      <input
+        className="form-control inputBuscar"
+        value={searchEmail}
+        onChange={handlesearcherEmail}
+        type="text"
+        placeholder="Búsqueda Por Email"
+      ></input>
+      <input
+        className="form-control inputBuscar"
+        value={searchPhone}
+        onChange={handlesearcherPhone}
+        type="text"
+        placeholder="Búsqueda Por Teléfono"
+      ></input>
+
+      <h3>Tabla de datos</h3>
+      <table>
+        <thead>
+          <tr>
+            <th>Nombre</th>
+            <th>Telefono</th>
+            <th>Edad</th>
+            <th>Email</th>
+            <th>Direccion</th>
+            <th>Fecha de Nacimiento</th>
+            <th>Acciones</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data &&
+            data.map((el) => (
+              <CrudTableRow
+                key={el.id}
+                el={el}
+                setDataToEdit={setDataToEdit}
+                deleteData={deleteData}
+              />
+            ))}
+        </tbody>
+      </table>
     </div>
   );
 };
